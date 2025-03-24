a5deea7c1648a1e4017f9f3d126aed6375d4cabf
rdbz23 <rayandbz9@gmail.com>
2025-03-24 01:03:29 -0400

    implementation de la liste des vols
    ajout de  activity_flight_list.xml
              AppDatabase.java
              build_gradle.kts
              Flight.java
              flight_item.xml
              FlightAdapter.java
              FlightDao.java
              FlightListActivity.java
    
    Aussi j fait queqlues modifs dans :  gradle.xml
                                                  libs.versions.toml
                                                  misc.xml
                                                  mais elles n'impactent aucunement le reste du projet

---
+++ app/src/main/java/com/example/applicationmobileairvip/FlightListActivity.java
@@ -0,0 +1,55 @@
+package com.example.applicationmobileairvip;
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.applicationmobileairvip.database.AppDatabase;
+import com.example.applicationmobileairvip.database.entities.Flight;
+import java.util.List;
+
+public class FlightListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FlightAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_list);
+
+
+    //on recup les parametres
+
+        String departure = getIntent().getStringExtra("DEPARTURE");
+        String arrival = getIntent().getStringExtra("ARRIVAL");
+        String date= getIntent().getStringExtra("DATE");
+
+        recyclerView = findViewById(R.id.flightRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        //charement depuis la Base de donnee
+        new LoadFlightTask().execute(departure, arrival, date);
+    }
+
+    private class LoadFlightTask extends AsyncTask<String, Void, List<Flight>> {
+        @Override
+        protected  List<Flight> doInBackgrount(String... params) {
+            return AppDatabase.getInstance(FlightListActivity.this).flightDao().searchFlights(params[0],params[1],params[2]);
+        }
+
+
+
+        @Override
+        protected void onPostExecute(List<Flight> flights){
+            adapter = new FlightAdapter(flights);
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+
+}
